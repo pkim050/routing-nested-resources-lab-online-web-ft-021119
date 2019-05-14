26aa1960189dd23bccc5b6069bdd92761de49cac
Patrick Kim <pkim050@gmail.com>
2019-05-14 13:53:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     @artist = Artist.find_by_id(params[:artist_id]) if params[:artist_id]
     @songs = @artist.songs if @artist
     @songs = Song.all
-    redirect_to artist_songs_path(@songs)
+    redirect_to artists_path
   end
 
   def show
